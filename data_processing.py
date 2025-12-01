a52c66a16e6cb9599e459760061e500f628d02fa
SannMyatMin <sanmyatmin88@gmail.com>
2025-12-01 11:39:40 +0630

    visualize data

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 class DataProcesser:
     def __init__(self, dataset_dir="data.pkl"):
@@ -21,3 +22,11 @@ class DataProcesser:
         test_y = np.array([td["label"] for td in testing_dataset])
 
         return train_x, train_y, test_x, test_y
+    
+    def display_img(self, data_x, data_y, img_num=5):
+        plt.figure(figsize=(20,20))
+        for i in range(img_num):
+            plt.subplot(1, img_num, i+1)
+            plt.title(f"Label - {data_y[i]}")
+            plt.imshow(data_x[i].reshape(28,28), cmap="gray")
+        plt.show()
